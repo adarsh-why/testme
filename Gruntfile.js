0605f6bfc057f8c284f555c92f7390b3ffefe5ff
Abdul <asalam@aconex.com>
2016-07-20 13:27:05 +0530

    Add karma test runner to the project
    Add 'karma' dependency
    Add 'grunt-karma'  tasks
    Load 'grunt-karma' tasks from Gruntfile
    Do minimal(but almost useless) karma task configuration in grunt
    
    At this stage it will just start the karma server and keep waiting, wondering what to do.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function(grunt){
+	grunt.initConfig({
+		karma : {
+			unit : {
+				    singleRun: true
+			}
+		}
+	});
 	grunt.registerTask('hello-world', function(){
 		console.log('hello world!');
 	});
 	grunt.registerTask('default', ['hello-world']);
+
+	grunt.loadNpmTasks('grunt-karma');
 };
